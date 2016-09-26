5e3c6f32ff37ed588c5e559c3aba35578f5e5fd3
Yaojie Lu <tanisraistlin@gmail.com>
2016-09-27 08:48:22 +1000

    1

--- Sensitivity Analysis/Psi50 - gs vs. PLC.r
+++ Sensitivity Analysis/Psi50 - gs vs. PLC.r
@@ -20,38 +20,47 @@ b <- 4.38
 kxmax <- 5
 c <- 2.64
 d <- 3.54
-h3 <- 10
 h <- l*a*LAI/nZ*p
 h2 <- l*LAI/nZ*p/1000
 gamma <- 1/((MAP/365/k)/1000)*nZ
 
-
 # Sensitivity Analysis
-SA <- seq(0.5, 2, by=0.1)*d
-data <- data.frame(gs50=numeric(length(SA)), PLC50=numeric(length(SA)))
+SA1 <- seq(0.5, 2, by=0.1)*d
+data <- data.frame(PLC50=numeric(length(SA1)), gs50=numeric(length(SA1)),
+                   PLC50=numeric(length(SA1)), gs50=numeric(length(SA1)),
+                   PLC50=numeric(length(SA1)), gs50=numeric(length(SA1)))
+SA2 <- c(10, 20, 40)
 
-for(i in 1:length(SA)){
-  d <- SA[i]
-  
-  g1 <- ESSf(1)
-  f1 <- function(w)ESSf(w)-g1*0.5
-  wL <- uniroot(ESSBf, c(0.12, 1), tol=.Machine$double.eps)$root
-  w50 <- uniroot(f1, c(wL, 1), tol=.Machine$double.eps)$root
-  data[i, 1] <- pxf(w50, ESSf(w50))
-  
-  data[i, 2] <- Psi50fd(d)
+for(i in 1:length(SA2)){
+  h3 <- SA2[i]
+  for(j in 1:length(SA1)){
+    d <- SA1[j]
+    
+    g1 <- ESSf(1)
+    f1 <- function(w)ESSf(w)-g1*0.4
+    wL <- uniroot(ESSBf, c(0.12, 1), tol=.Machine$double.eps)$root
+    w60 <- uniroot(f1, c(wL, 1), tol=.Machine$double.eps)$root
+    data[j, 2*i-1] <- Psi50fd(d)
+    
+    data[j, 2*i] <- pxf(w60, ESSf(w60))
+  }
 }
 
 # Figures
+Cols <- c("black", "blue", "red")
 windows(8, 6)
 par(mgp=c(2.2, 1, 0), xaxs="i", yaxs="i", lwd=2, mar=c(3.9, 3.9, 1, 1), mfrow=c(1,1))
-plot(data,
+plot(data[1:2],
      xaxt="n", yaxt="n", xlab=NA, ylab=NA,
      xlim=c(-7, 0), ylim=c(-7, 0),
-     cex.lab=1.3, type="l")
+     cex.lab=1.3, type="l", col=Cols[1])
+points(data[3:4], type="l", col=Cols[2])
+points(data[5:6], type="l", col=Cols[3])
 
 axis(1, xlim=c(-7, 0), pos=-7, lwd=2)
-mtext(expression(italic(psi[x50*", "*g[s]])~(MPa)),side=1,line=2.4, cex=1.3)
+mtext(expression(italic(psi[x50*", "*PLC])~(MPa)),side=1,line=2.4, cex=1.3)
 axis(2, ylim=c(-7, 0), pos=-7, lwd=2)
-mtext(expression(italic(psi[x50*", "*PLC])~(MPa)),side=2,line=1.8, cex=1.3)
-dev.copy2pdf(file = "Figures/gs50 - PLC50.pdf")
+mtext(expression(italic(psi[x60*", "*g[s]])~(MPa)),side=2,line=1.8, cex=1.3)
+abline(a=0, b=1, lwd=1, lty=2)
+legend("topleft", c("10", "20", "40"), title=expression(italic(h[3])), lty=c(1, 1, 1), col=Cols)
+dev.copy2pdf(file = "Figures/gs60 - PLC50.pdf")
--- Sensitivity Analysis/Psimin vs. Psi50.r
+++ Sensitivity Analysis/Psimin vs. Psi50.r
@@ -20,7 +20,6 @@ b <- 4.38
 kxmax <- 5
 c <- 2.64
 d <- 3.54
-h3 <- 10
 h <- l*a*LAI/nZ*p
 h2 <- l*LAI/nZ*p/1000
 gamma <- 1/((MAP/365/k)/1000)*nZ
@@ -28,28 +27,57 @@ gamma <- 1/((MAP/365/k)/1000)*nZ
 
 # Sensitivity Analysis
 SA <- seq(0.5, 2, by=0.1)*d
-data <- data.frame(PLC50=numeric(length(SA)), PLCmin=numeric(length(SA)))
 
+# h3 = 10
+h3 <- 10
+data10 <- data.frame(PLC50=numeric(length(SA)), PLCmin=numeric(length(SA)))
 for(i in 1:length(SA)){
   d <- SA[i]
   
-  data[i, 1] <- Psi50fd(d)
+  data10[i, 1] <- Psi50fd(d)
   
   wL <- uniroot(ESSBf, c(0.12, 1), tol=.Machine$double.eps)$root
-  data[i, 2] <- pxf(wL, ESSf(wL))
+  data10[i, 2] <- pxf(wL, ESSf(wL))
+}
+
+# h3 = 20
+h3 <- 20
+data20 <- data.frame(PLC50=numeric(length(SA)), PLCmin=numeric(length(SA)))
+for(i in 1:length(SA)){
+  d <- SA[i]
+  
+  data20[i, 1] <- Psi50fd(d)
+  
+  wL <- uniroot(ESSBf, c(0.12, 1), tol=.Machine$double.eps)$root
+  data20[i, 2] <- pxf(wL, ESSf(wL))
+}
+# h3 = 40
+h3 <- 40
+data40 <- data.frame(PLC50=numeric(length(SA)), PLCmin=numeric(length(SA)))
+for(i in 1:length(SA)){
+  d <- SA[i]
+  
+  data40[i, 1] <- Psi50fd(d)
+  
+  wL <- uniroot(ESSBf, c(0.12, 1), tol=.Machine$double.eps)$root
+  data40[i, 2] <- pxf(wL, ESSf(wL))
 }
 
 # Figures
+Cols <- c("black", "blue", "red")
 windows(8, 6)
 par(mgp=c(2.2, 1, 0), xaxs="i", yaxs="i", lwd=2, mar=c(3.9, 3.9, 1, 1), mfrow=c(1,1))
-plot(data,
+plot(data10,
      xaxt="n", yaxt="n", xlab=NA, ylab=NA,
      xlim=c(-10, 0), ylim=c(-10, 0),
-     cex.lab=1.3, type="l")
+     cex.lab=1.3, type="l", col=Cols[1])
+points(data20, type="l", col=Cols[2])
+points(data40, type="l", col=Cols[3])
 
 axis(1, xlim=c(-10, 0), pos=-10, lwd=2)
 mtext(expression(italic(psi[x50])~(MPa)),side=1,line=2.4, cex=1.3)
 axis(2, ylim=c(-10, 0), pos=-10, lwd=2)
 mtext(expression(italic(psi[xmin])~(MPa)),side=2,line=1.8, cex=1.3)
-abline(a=0, b=1, lty=2)
+abline(a=0, b=1, lwd=1, lty=2)
+legend("topleft", c("10", "20", "40"), title=expression(italic(h[3])), lty=c(1, 1, 1), col=Cols)
 dev.copy2pdf(file = "Figures/Psimin - Psi50.pdf")
